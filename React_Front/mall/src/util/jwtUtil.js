5c4635a00eca8e10f239986a682fccb1c743dfd8
7532602 <7532602@gmail.com>
2024-08-05 23:22:23 +0900

    refershToken 을 이용한 accessToken 재발급

--- React_Front/mall/src/util/jwtUtil.js
+++ React_Front/mall/src/util/jwtUtil.js
@@ -1,7 +1,20 @@
 import axios from "axios";
-import { getCookie } from "./cokkieUtil";
+import { getCookie, setCookie } from "./cokkieUtil";
+import { API_SERVER_HOST } from "../api/todoApi";
 
 const jwtAxios = axios.create();
+
+const refreshJWT = async (accessToken, refreshToken) => {
+  const host = API_SERVER_HOST;
+
+  const header = { headers: { Authorization: `Bearer ${accessToken}` } };
+
+  const res = await axios.get(
+    `${host}/api/member/refresh?refreshToken=${refreshToken}`,
+    header
+  );
+  return res.data;
+};
 
 const beforeReq = (config) => {
   console.log("before request.............");
@@ -23,6 +36,29 @@ const requestFail = (err) => {
 //before return response
 const beforeRes = async (res) => {
   console.log("before return response...........");
+
+  const data = res.data;
+
+  if (data && data.error === "ERROR_ACCESS_TOOKEN") {
+    const memberCookieValue = getCookie("member");
+
+    const result = await refreshJWT(
+      memberCookieValue.accessToken,
+      memberCookieValue.refreshToken
+    );
+
+    // 새로운 accessToken, refreshToken
+    memberCookieValue.accessToken = result.accessToken;
+    memberCookieValue.refreshToken = result.refreshToken;
+
+    setCookie("member", JSON.stringify(memberCookieValue), 10);
+
+    const originalRequest = res.config;
+
+    originalRequest.headers.Authorization = `Bearer ${result.accessToken}`;
+
+    return await axios(originalRequest);
+  }
   return res;
 };
 //fail response

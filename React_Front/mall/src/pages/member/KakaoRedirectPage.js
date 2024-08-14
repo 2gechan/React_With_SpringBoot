101e481aa696b15ed70f58e85d28f8aa86acbbc0
7532602 <7532602@gmail.com>
2024-08-14 22:55:20 +0900

    소셜 로그인 사용자 자동 회원가입

--- React_Front/mall/src/pages/member/KakaoRedirectPage.js
+++ React_Front/mall/src/pages/member/KakaoRedirectPage.js
@@ -1,18 +1,32 @@
 import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import { getAccessToken, getMemberWithAccessToken } from "../../api/kakaoApi";
+import { useDispatch } from "react-redux";
+import { login } from "../../slices/loginSlice";
+import useCustomLogin from "../../hooks/useCustomLogin";
 
 const KakaoRedirectPage = () => {
   const [searchParams] = useSearchParams();
   const authCode = searchParams.get("code");
 
+  const dispatch = useDispatch();
+
+  const { moveToPath } = useCustomLogin();
+
   useEffect(() => {
     getAccessToken(authCode).then((data) => {
       const accessToken = data;
 
-      getMemberWithAccessToken(accessToken).then((result) => {
+      getMemberWithAccessToken(accessToken).then((memberInfo) => {
         console.log("==================");
-        console.log(result);
+        console.log(memberInfo);
+        dispatch(login(memberInfo));
+
+        if (memberInfo && memberInfo.social) {
+          moveToPath("/member/modify");
+        } else {
+          moveToPath("/");
+        }
       });
     });
   }, [authCode]);
--- SpringBoot_Back/mallapi/src/main/java/com/gechan/mallapi/controller/SocialController.java
+++ SpringBoot_Back/mallapi/src/main/java/com/gechan/mallapi/controller/SocialController.java
@@ -1,10 +1,14 @@
 package com.gechan.mallapi.controller;
 
+import com.gechan.mallapi.dto.MemberDTO;
 import com.gechan.mallapi.service.MemberService;
+import com.gechan.mallapi.util.JWTUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @Log4j2
@@ -14,11 +18,18 @@ public class SocialController {
     private final MemberService memberService;
 
     @GetMapping("/api/member/kakao")
-    public String[] getMemberFromKakao(String accessToken) {
+    public Map<String, Object> getMemberFromKakao(String accessToken) {
 
         log.debug("accessToken : {}", accessToken);
-        memberService.getKakaoMember(accessToken);
+        MemberDTO memberDTO = memberService.getKakaoMember(accessToken);
 
-        return new String[]{"AAA", "BBB"};
+        Map<String, Object> claims = memberDTO.getClaims();
+
+        String jwtAccessToken = JWTUtil.generateToken(claims, 10);
+        String jwtRefreshToken = JWTUtil.generateToken(claims, 60 * 24);
+
+        claims.put("accessToken", jwtAccessToken);
+        claims.put("refreshToken", jwtRefreshToken);
+        return claims;
     }
 }
